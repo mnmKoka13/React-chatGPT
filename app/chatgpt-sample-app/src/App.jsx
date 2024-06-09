9914a3ce94f1d07f372a787481f41c2a378663a6
Manami Kokado <kokado313.mnm@gmail.com>
2024-06-09 16:48:08 +0900

    ChatGPT APIとの通信を実装、CSSを修正

--- app/chatgpt-sample-app/src/App.jsx
+++ app/chatgpt-sample-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [prompt, setPrompt] = useState("");
@@ -7,7 +8,35 @@ function App() {
   const [response, setResponse] = useState("");
   const API_KEY = import.meta.env.VITE_API_KEY;
 
-  const handlePrompt = async () => {console.log("prompt: ", prompt);};
+  const handlePrompt = async () => {
+    try {
+      setResponse("Loading...");
+      setQuestion(prompt);
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: prompt,
+            }
+          ]
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+          },
+        }
+      );
+      console.log(response);
+      setResponse(response.data.choices[0].message.content);
+      setPrompt("");
+    } catch (error) {
+      setResponse("Error ChatGPT: ${error}");
+    }
+  };
 
   return (
     <>
@@ -19,14 +48,15 @@ function App() {
         <div className="message-form">
           <input
             type="text"
+            className="message-input"
             value={prompt}
             placeholder="Please input prompt..."
             onChange={(e) => setPrompt(e.target.value)}
           />
-          <button onClick={() => handlePrompt()}>Ask</button>
+          <button className="send-button" onClick={() => handlePrompt()}>Ask</button>
         </div>
 
-        <section className="chat-message">
+        <section id="chat-message" className="chat-message">
           <div className="message user-message">
             <div className="text">{question}</div>
           </div>
